87903131ce6195b9ae6a16ae77783b34155d946f
Luktronic <luka.furundzija@gmx.at>
2024-04-18 10:57:59 +0200

    Initial Commit

---
+++ src/client/java/eu/kekx/MedievalFlintClient.java
@@ -0,0 +1,10 @@
+package eu.kekx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MedievalFlintClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
